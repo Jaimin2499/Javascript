162113863660b8f710c384f2859f7181e4912cb3
Jaimin2499 <58000318+Jaimin2499@users.noreply.github.com>
2021-05-07 17:10:25 +0530

    Javascript (Demo)

---
+++ Demo.js
@@ -0,0 +1,10 @@
+function img1() {
+    document.getElementById('trip').src = "https://media1.tenor.com/images/6d46f99196e19ecd1167ec20bf3d7244/tenor.gif?itemid=6217909";
+
+
+}
+
+function img2() {
+    document.getElementById('trip').src = "https://t3.ftcdn.net/jpg/02/68/63/18/360_F_268631816_eEVxIDmTnltGayGPKROXNx4Xt5SNmzuH.jpg";
+
+}
